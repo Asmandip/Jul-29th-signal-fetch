ef12db86cf5a1ac48fb00677c804ae08a4fd4c85
Asmandip <asmandip@gmail.com>
2025-07-29 11:57:00 +0530

    utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,12 @@
-# utils.py content
+from datetime import datetime
+
+def format_symbol(symbol):
+    """Standardize symbol format."""
+    return symbol.upper().replace("_UMCBL", "")
+
+def get_current_time():
+    return datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S UTC")
+
+def percent_change(open_price, close_price):
+    """Calculate percentage change between two prices."""
+    return round(((close_price - open_price) / open_price) * 100, 2)
